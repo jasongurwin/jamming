1b7dfa85b902115e9fe6acb90a6cad6869e8aabe
jasongurwin <>
2017-10-19 17:17:42 -0700

    updated through step 34

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,14 +5,19 @@ import SearchResults from '../SearchResults/SearchResults'
 import Playlist from '../Playlist/Playlist'
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+  this.state = {SearchResults: [{name:'Tiny Dancer Live', artist: 'Ben Folds', album: 'Ben Folds Live'},{name:'Tiny Dancer', artist: 'Elton John', album: 'Madman Across The Water'}]};
+  }
+
   render() {
     return (
       <div>
-  <h1>Ja<span class="highlight">mmm</span>ing</h1>
+  <h1>Ja<span className="highlight">mmm</span>ing</h1>
   <div className="App">
     <SearchBar />
     <div className="App-playlist">
-      <SearchResults />
+      <SearchResults SearchResults={this.state.SearchResults}/>
       <Playlist />
     </div>
   </div>
--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -7,9 +7,9 @@ class SearchResults extends React.Component{
   render(){
     return(
       <div className="SearchResults">
-  <h2>Results</h2>
-  <TrackList />
-</div>
+        <h2>Results</h2>
+        <TrackList tracks={this.props.SearchResults}/>
+      </div>
 
     );
   }
