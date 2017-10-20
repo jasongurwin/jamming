7ffcdbf5af191a3d198aeb8146d9df57dd7932a0
jasongurwin <>
2017-10-20 14:30:06 -0700

    added add Track

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -9,13 +9,13 @@ class App extends React.Component {
     super(props);
     this.state = {SearchResults: [{name:'Tiny Dancer Live', artist: 'Ben Folds', album: 'Ben Folds Live'},{name:'Tiny Dancer', artist: 'Elton John', album: 'Madman Across The Water'}],Playlist:{playlistName: 'Jasons Playlist',playlistTracks:[{name:'Yellow', artist: 'Coldplay', album: 'Over The Water'},{name:'Piano Man', artist: 'Billy Joel', album: 'The Piano Man'}]} };
     this.addTrack = this.addTrack.bind(this);
-  }
+  };
 
   addTrack(track){
-    if (track.id == false){
+    if (track.id === false){
       this.state.Playlist.playlistTracks.push(track)
     }
-  }
+  };
 
   render() {
     return (
@@ -24,7 +24,7 @@ class App extends React.Component {
   <div className="App">
     <SearchBar />
     <div className="App-playlist">
-      <SearchResults onAdd={addTrack()} SearchResults={this.state.SearchResults}/>
+      <SearchResults onAdd={this.addTrack()} SearchResults={this.state.SearchResults}/>
       <Playlist Playlist={this.state.Playlist}/>
     </div>
   </div>
